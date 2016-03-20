4fb2331556561cc74d332085db84e563b358969d
Rizart Dokollari <r.dokollari@gmail.com>
2016-03-20 04:44:39 +0200

    Script: Enable player moving, rotating, and animating.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,62 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float Speed = 6f;
+
+    Vector3 _movement;
+    Animator _animator;
+    Rigidbody _playeRigidbody;
+    int _floorMask;
+    float _camRayLength = 100f;
+
+    private void Awake()
+    {
+        _floorMask = LayerMask.GetMask("Floor");
+        _animator = GetComponent<Animator>();
+        _playeRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        var horizontalAxis = Input.GetAxisRaw("Horizontal");
+        var verticalAxis = Input.GetAxisRaw("Vertical");
+
+        Move(horizontalAxis, verticalAxis);
+        Turning();
+        Animating(horizontalAxis, verticalAxis);
+    }
+
+    private void Move(float horizontalAxis, float verticalAxis)
+    {
+        _movement.Set(horizontalAxis, 0, verticalAxis);
+
+        _movement = _movement.normalized * Speed * Time.deltaTime;
+
+        _playeRigidbody.MovePosition(transform.position + _movement);
+    }
+
+    private void Turning()
+    {
+        var camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorRaycastHit;
+
+        if (!Physics.Raycast(camRay, out floorRaycastHit, _camRayLength, _floorMask)) return;
+
+        var playerToMouse = floorRaycastHit.point - transform.position;
+        playerToMouse.y = 0f;
+
+        var newRotationQuaternion = Quaternion.LookRotation(playerToMouse);
+        _playeRigidbody.MoveRotation(newRotationQuaternion);
+    }
+
+    private void Animating(float horizontalAxis, float verticalAxis)
+    {
+        const float tolerance = 0f;
+        var walking = Math.Abs(horizontalAxis) > tolerance || Math.Abs(verticalAxis) > tolerance;
+
+        _animator.SetBool("IsWalking", walking);
+    }
 }
